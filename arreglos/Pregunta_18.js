2b369f9dc471a72bf419dff2ffa457f54190b51a
Jchv0112 <jcarloshdez0112@gmail.com>
2023-04-19 17:24:02 -0500

    Agrego carpeta arreglos con Pregunta 18

---
+++ arreglos/Pregunta_18.js
@@ -0,0 +1,32 @@
+let num =[7,7,7];
+var puntaje_total =0;
+
+for (let numeros of num) {
+    pares (numeros);
+    impares (numeros);
+    siete (numeros);
+
+}
+console.log (puntaje_total);
+
+
+function pares (num) {
+    if (num%2 ===0)
+    {
+        puntaje_total+=2;
+
+    }
+}
+function impares (num){
+    if (num%2 != 0  && num!=7)
+    {
+        puntaje_total++;
+    }
+}
+function siete (num)
+{
+    if (num === 7)
+    {
+        puntaje_total+=3;
+    }
+}
